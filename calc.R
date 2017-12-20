b46ca374f12ee2e8c31e12bda29795b896496c05
anshmalik <34131246+anshmalik@users.noreply.github.com>
2017-12-20 12:52:57 +0530

    Add files via upload

--- calc.R
+++ calc.R
@@ -1,39 +1,28 @@
 calculator<-function(x)
   {
-    
+    a<-readline(prompt = "Enter first number: ")
+    a<-as.integer(a)
+    b<-readline(prompt = "Enter second number: ")
+    b<-as.integer(b)
     if(x == 1)
     {
-      a<-readline(prompt = "Enter first number: ")
-      a<-as.integer(a)
-      b<-readline(prompt = "Enter second number: ")
-      b<-as.integer(b)
       print("Sum:-")
       print(a+b)
     }
     else if(x == 2)
     {
-      a<-readline(prompt = "Enter first number: ")
-      a<-as.integer(a)
-      b<-readline(prompt = "Enter second number: ")
-      b<-as.integer(b)
       print("Difference:-")
       print(a-b)
     }
     else if(x == 3)
     {
-      a<-readline(prompt = "Enter first number: ")
-      a<-as.integer(a)
-      b<-readline(prompt = "Enter second number: ")
-      b<-as.integer(b)
+      
       print("Product:-")
       print(a*b)
     }
     else if(x == 4)
     {
-      a<-readline(prompt = "Enter first number: ")
-      a<-as.integer(a)
-      b<-readline(prompt = "Enter second number: ")
-      b<-as.integer(b)
+      
       print("Quotient:-")
       print(a/b)
     }
